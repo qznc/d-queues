72afacf11c1e817bba1dfd1ac41f4ffafbedbad5
Andreas Zwinkau <zwinkau@mailbox.org>
2017-10-22 19:57:05 +0200

    Benchmark like Maged and Michael

--- source/app.d
+++ source/app.d
@@ -3,6 +3,15 @@ import std.experimental.checkedint;
 
 import queues;
 
+void busy_work() {
+    // assume compiler is unable to evaluate this at compile time
+    import std.random;
+    auto gen = Mt19937(11);
+    foreach(x;gen) {
+        if (x < 80000) break;
+    }
+}
+
 /***
   Start `writers` amount of threads to write into a queue.
   Start `readers` amount of threads to read from the queue.
@@ -33,6 +42,7 @@ void test_run(alias Q)(uint writers, uint readers, uint count)
                         b.wait();
                         foreach(n; 0 .. items_per_writer) {
                             q.enqueue(n);
+                            busy_work();
                         }
                         });
                 t.start();
@@ -49,6 +59,7 @@ void test_run(alias Q)(uint writers, uint readers, uint count)
                         foreach(_; 0 .. items_per_reader) {
                             auto n = q.dequeue();
                             s += n;
+                            busy_work();
                         }
                         synchronized { sum += s; }
                         });
@@ -64,20 +75,54 @@ void test_run(alias Q)(uint writers, uint readers, uint count)
     assert(sum == correct_sum);
 }
 
+enum MagedMichaelOpCount = 100_000; /* 1_000_000 in paper */
+/***
+  Benchmark like Maged and Michael.
+*/
+void test_run2(Queue!long q, uint threads)
+{
+    import core.thread;
+    import core.sync.barrier : Barrier;
+    import std.bigint : BigInt;
+    const count = MagedMichaelOpCount / threads;
+    Thread[] ts;
+    foreach(i; 0 .. threads) {
+        auto t = new Thread({
+                foreach(n; 0 .. count) {
+                    q.enqueue(n);
+                    busy_work();
+                    auto x = q.dequeue();
+                    busy_work();
+                }
+                });
+        t.start();
+        ts ~= t;
+    }
+    foreach(t; ts) { t.join(); }
+}
+
 void main(string[] args) {
     import std.datetime.stopwatch : benchmark;
     enum readers = 2;
-    enum writers = 2;
+    enum writers = 3;
     enum bnd = 10; // size for bounded queues
     enum count = 100000;
-    void f0() { test_run!(NaiveThreadsafeQueue!long)              (writers,readers,count); }
-    void f1() { test_run!(NaiveBoundedThreadsafeQueue!(long,bnd)) (writers,readers,count); }
-    void f2() { test_run!(MagedBlockingQueue!long)                (writers,readers,count); }
-    void f3() { test_run!(MagedNonBlockingQueue!long)             (writers,readers,count); }
-    auto r = benchmark!(f0, f1, f2, f3)(3);
+    void baseline() {
+        /* purely the busy work of one thread */
+        foreach(_; 0..MagedMichaelOpCount / writers) {
+            busy_work();
+            busy_work();
+        }
+    }
+    void f0() { test_run2(new NaiveThreadsafeQueue!(long)(),            writers); }
+    void f1() { test_run2(new NaiveBoundedThreadsafeQueue!(long,bnd)(), writers); }
+    void f2() { test_run2(new MagedBlockingQueue!long(),                writers); }
+    void f3() { test_run2(new MagedNonBlockingQueue!long(),             writers); }
+    auto r = benchmark!(baseline, f0, f1, f2, f3)(3);
     import std.stdio;
-    writeln(r[0]);
-    writeln(r[1]);
-    writeln(r[2]);
-    writeln(r[3]);
+    auto base = r[0];
+    writeln(r[1] - base);
+    writeln(r[2] - base);
+    writeln(r[3] - base);
+    writeln(r[4] - base);
 }
